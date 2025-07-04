e22412466e46afe30b72ac3470645dfcf12f253e
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-07-04 22:01:45 +0200

    Basic Sanity + Astro Oppsett

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "531mn2v8",
+      dataset: "production",
+    }),
+    react(),
+  ],
+});
