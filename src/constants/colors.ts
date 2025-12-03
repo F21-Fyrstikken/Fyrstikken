3a38cb4114bae7d486b747bd4868701588f036ea
Zandra7 <sandra.eng@live.no>
2025-12-03 17:41:46 +0100

    Flytter farger til egen fil

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+export const colors = [
+  { light: "var(--light-safir)", dark: "var(--dark-safir)" },
+  { light: "var(--light-akvamarin)", dark: "var(--dark-akvamarin)" },
+  { light: "var(--light-amazonitt)", dark: "var(--dark-amazonitt)" },
+  { light: "var(--light-smaragd)", dark: "var(--dark-smaragd)" },
+  { light: "var(--light-citrin)", dark: "var(--dark-citrin)" },
+  { light: "var(--light-spessartin)", dark: "var(--dark-spessartin)" },
+  { light: "var(--light-rubin)", dark: "var(--dark-rubin)" },
+  { light: "var(--light-thulitt)", dark: "var(--dark-thulitt)" },
+  { light: "var(--light-kunzitt)", dark: "var(--dark-kunzitt)" },
+  { light: "var(--light-ametyst)", dark: "var(--dark-ametyst)" },
+];
